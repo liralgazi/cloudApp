6f7180caa466e15481cbe4eb11f11fd9cfb8b453
Lir Algazi <liralgazi@gmail.com>
2023-07-06 22:09:26 +0300

    db changes

---
+++ server/index.js
@@ -0,0 +1,100 @@
+import express from "express";
+import mysql from "mysql";
+
+//import cors from "cors";
+
+const app = express();
+//app.use(cors());
+app.use(express.json());
+
+
+app.listen(3010, () => {
+    console.log("Connected to backend.");
+  });
+  
+const db = mysql.createConnection({
+    /*
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  */
+ host: "localhost",
+ user: "root",
+ password: "La2101_99",
+ database: "app",
+});
+
+/*
+connection.connect((err) => {
+    if (err)
+        console.log(err.message);
+    console.log('db '+ connection.state);
+});
+*/
+
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
+
+app.get("/books", (req, res) => {
+    const q = "SELECT * FROM books";
+    db.query(q, (err, data) => {
+      if (err) {
+        console.log(err);
+        return res.json(err);
+      }
+      return res.json(data);
+    });
+  });
+
+  app.post("/books", (req, res) => {
+    const q = "INSERT INTO books(`title`, `desc`, `price`, `cover`) VALUES (?)";
+  
+    const values = [
+      req.body.title,
+      req.body.desc,
+      req.body.price,
+      req.body.cover,
+    ];
+  
+    db.query(q, [values], (err, data) => {
+      if (err) return res.send(err);
+      return res.json(data);
+    });
+  });
+
+
+/*
+
+
+
+app.delete("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = " DELETE FROM books WHERE id = ? ";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.put("/books/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "UPDATE books SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [...values,bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+*/
