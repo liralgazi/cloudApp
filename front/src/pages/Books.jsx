b6194c1f3c83c8c80ed5f5a0e7b04134f462a031
Lir Algazi <liralgazi@gmail.com>
2023-07-09 01:43:15 +0300

    .

--- front/src/pages/Books.jsx
+++ front/src/pages/Books.jsx
@@ -10,7 +10,7 @@ const Books = () => {
   useEffect(() => {
     const fetchAllBooks = async () => {
       try {
-        const res = await axios.get("http://13.49.0.0:3010/books");
+        const res = await axios.get("http://13.53.141.203:3010/books");
         setBooks(res.data);
       } catch (err) {
         console.log(err);
@@ -23,7 +23,7 @@ const Books = () => {
 
   const handleDelete = async (id) => {
     try {
-      await axios.delete(`http://13.49.0.0:3010/books/${id}`);
+      await axios.delete(`http://13.53.141.203:3010/books/${id}`);
       window.location.reload()
     } catch (err) {
       console.log(err);
