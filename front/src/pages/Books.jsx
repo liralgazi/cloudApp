21c6dc323f17dda89b22316ca202f239793e54a7
Lir Algazi <liralgazi@gmail.com>
2023-07-22 19:50:26 +0300

    .

--- front/src/pages/Books.jsx
+++ front/src/pages/Books.jsx
@@ -10,8 +10,8 @@ const Books = () => {
   useEffect(() => {
     const fetchAllBooks = async () => {
       try {
-        //const res = await axios.get("http://final-project-LB-142331911.eu-north-1.elb.amazonaws.com/books");
-        const res = await axios.get("http://13.53.141.203:3010/books");
+        const res = await axios.get("http://final-project-LB-142331911.eu-north-1.elb.amazonaws.com/books");
+        //const res = await axios.get("http://13.53.141.203:3010/books");
         setBooks(res.data);
       } catch (err) {
         console.log(err);
@@ -24,8 +24,8 @@ const Books = () => {
 
   const handleDelete = async (id) => {
     try {
-      //await axios.delete(`http://final-project-LB-142331911.eu-north-1.elb.amazonaws.com/books/${id}`);
-      await axios.delete(`http://13.53.141.203:3010/books/${id}`);
+      await axios.delete(`http://final-project-LB-142331911.eu-north-1.elb.amazonaws.com/books/${id}`);
+      //await axios.delete(`http://13.53.141.203:3010/books/${id}`);
       window.location.reload()
     } catch (err) {
       console.log(err);
