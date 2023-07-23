6a016b94f79999b49a0fbb027329622ce6fbec6c
Lir Algazi <liralgazi@gmail.com>
2023-07-23 20:27:58 +0300

    .

--- front/src/pages/Books.jsx
+++ front/src/pages/Books.jsx
@@ -10,17 +10,18 @@ const Books = () => {
   useEffect(() => {
     const fetchAllBooks = async () => {
       try {
-        
+        /*
         const res = await axios.get("final-project-LB-142331911.eu-north-1.elb.amazonaws.com:3010")
         .then(res => {
           console.log("res" , res);
           console.log('hello');
           setBooks(res.data);
         });   
-            /*
-        const res = await axios.get("13.53.141.203:3010/books");
-        setBooks(res.data);
         */
+            
+        const res = await axios.get("http://13.53.141.203:3010/books");
+        setBooks(res.data);
+        
       } catch (err) {
         console.log(err);
       }
