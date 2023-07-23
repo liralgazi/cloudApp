dad7ff812e60e9997b6333f88d3816c1d8979439
shir moskovits <93382306+ShirMosko@users.noreply.github.com>
2023-07-23 20:45:43 +0300

    try another way to connect to axios

--- front/src/pages/Books.jsx
+++ front/src/pages/Books.jsx
@@ -3,31 +3,53 @@ import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
 import { Link } from "react-router-dom";
+import { getBooks } from "../api/api";
 
 const Books = () => {
   const [books, setBooks] = useState([]);
 
-  useEffect(() => {
-    const fetchAllBooks = async () => {
-      try {
-        /*
-        const res = await axios.get("final-project-LB-142331911.eu-north-1.elb.amazonaws.com:3010")
-        .then(res => {
-          console.log("res" , res);
-          console.log('hello');
-          setBooks(res.data);
-        });   
-        */
-            
-        const res = await axios.get("http://13.53.141.203:3010/books");
-        setBooks(res.data);
-        
-      } catch (err) {
-        console.log(err);
+
+  const getAllBooks = React.useCallback(async () => {
+    try {
+      const { data } = await getBooks();
+
+      if (data?.code !== 200) {
+        throw new Error();
       }
-    };
-    fetchAllBooks();
-  }, []);
+      setBooks(data);
+    } catch (error) {
+      return;
+    } finally {
+    
+    }
+  }, [books]);
+
+  useEffect(() => {
+    getAllBooks();
+  }, [getAllBooks]);
+
+
+  // useEffect(() => {
+  //   const fetchAllBooks = async () => {
+  //     try {
+  //       /*
+  //       const res = await axios.get("final-project-LB-142331911.eu-north-1.elb.amazonaws.com:3010")
+  //       .then(res => {
+  //         console.log("res" , res);
+  //         console.log('hello');
+  //         setBooks(res.data);
+  //       });   
+  //       */
+            
+  //       const res = await axios.get("http://13.53.141.203:3010/books");
+  //       setBooks(res.data);
+        
+  //     } catch (err) {
+  //       console.log(err);
+  //     }
+  //   };
+  //   fetchAllBooks();
+  // }, []);
 
   console.log(books);
 
