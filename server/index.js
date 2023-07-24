724a5fd2532e996446e6a351aa9bb87d7bf1e68d
Lir Algazi <liralgazi@gmail.com>
2023-07-24 22:41:32 +0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,6 +10,15 @@ app.use(cors({
   origin: '*',
 }));
 
+
+// Add CORS middleware to allow all origins
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(express.json());
 
 const db = mysql.createConnection({
