447984cf681b69c1f5a60f4f92f4d5e3195ad64c
Lir Algazi <liralgazi@gmail.com>
2023-07-24 18:13:40 +0300

    Update setupProxy.js

--- front/setupProxy.js
+++ front/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/books',
     createProxyMiddleware({
-      target: 'http://13.51.234.120:3000',
+      target: 'http://13.51.234.120:3010',
       changeOrigin: true,
     })
   );
