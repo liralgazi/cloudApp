0cb95009c1fbe21e7c80a80e2f262acde7d69a6a
Lir Algazi <liralgazi@gmail.com>
2023-07-24 22:45:41 +0300

    Update setupProxy.js

--- front/setupProxy.js
+++ front/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function(app) {
   app.use(
     '/books',
     createProxyMiddleware({
-      target: 'http://cloud-project-lb-1993630990.eu-north-1.elb.amazonaws.com::3010',
+      target: 'https://http://cloud-project-LB-1993630990.eu-north-1.elb.amazonaws.com:3010/books',
       changeOrigin: true,
     })
   );
